1f8b303cd46bbac2333fcada2b997dd2b9d459cb
agathalindemberg-Acs <135237010+agathalindemberg-Acs@users.noreply.github.com>
2023-06-14 14:17:23 -0300

    Add files via upload

--- projetoTemplateDrivenForms/src/app/app.component.ts
+++ projetoTemplateDrivenForms/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
+class Cliente{
+  nome: string;
+  email: string;
+  profissao: '';
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projetoTemplateDrivenForms';
+
+  cliente = new Cliente();
+  profissoes = ['Programador', 'Empresario', 'Outra'];
+
+  salvar(form: NgForm){
+    //this.cliente.nome = form.value.primeiroNome;
+    //this.cliente.email = form.value.emailAddress;
+    //this.cliente.profissao = form.value.profissao;
+    console.log(form);
+    console.log(this.cliente);
+  
+    form.reset({profissao:''});
+  }
+
+
+
 }
